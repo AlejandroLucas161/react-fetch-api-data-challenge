c73a6bb1c813c6c2ace0b9530601469fe60ff1ea
AlejandroLucas161 <alejandrolucas161@gmail.com>
2022-01-12 14:43:34 -0300

    Part A of the challenge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import react from "react";
+import { useState, useEffect } from 'react';
+import Form from './components/Form';
+import List from './components/List';
 
 function App() {
-  return <div className="App"></div>;
+  const API_URL = 'https://jsonplaceholder.typicode.com/';
+  const [reqType, setReqType] = useState('users');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}${reqType}`);
+        const data = await response.json();
+        setItems(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchItems();
+  }, [reqType]);
+
+  return (
+    <div className='App'>
+      <Form reqType={reqType} setReqType={setReqType} />
+      <List items={items} />
+    </div>
+  );
 }
 
 export default App;
